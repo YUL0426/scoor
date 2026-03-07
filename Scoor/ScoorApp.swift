1ad29efbab294ffe806d78b958087b651db36cf3
YUL0426 <yulheo97@gmail.com Primary Verified Private Connected to Googleyulheo97@gmail.com>
2026-03-07 13:19:15 +0900

    Initial Commit

---
+++ Scoor/ScoorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoorApp.swift
+//  Scoor
+//
+//  Created by mac on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ScoorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
